00fbb392abf666c3ac68947d24540390f70afaef
ByMystick <16-@bk.ru>
2023-03-23 15:09:08 +0300

    step2 complet

--- Makefile
+++ Makefile
@@ -3,16 +3,15 @@ install:
 
 build:
 	poetry build
-	
+
 publish:
 	poetry publish --dry-run
-	
+
 package-install:
 	python3 -m pip install --user dist/*.whl
 
 lint:
 	poetry run flake8 gendiff
-	
-gendiff:
-	poetry run gendiff
 
+gendiff:
+	poetry run gendiff -h
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,10 +5,12 @@ import argparse
 
 def main():
     """display help information"""
-    parser = argparse.ArgumentParser(prog='gendiff',
-                                     description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(description='Compares '
+                                                 'two configuration files and '
+                                                 'shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument("-f", "--format", help="set format of output")
     parser.print_help()
 
 
